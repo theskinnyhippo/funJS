94e0ed41579429b0ba8d228793f5fcdca93e604b
theskinnyhippo <134091490+theskinnyhippo@users.noreply.github.com>
2024-06-30 23:08:10 +0000

    nicely done

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const acc_id = 65699556
+let acc_email = "mandirhapal@google.com"
+var acc_password = "14192"
+acc_city = "Hyderabad"
+
+//acc_id = 10833558     not allowed
+
+console.log(acc_id)
+console.log(acc_email)
+console.log(acc_password)
+console.log(acc_city)
+
+console.table([acc_id,acc_email,acc_password,acc_city])
+
+
+//js didnt have the control of scope --> {}
