79c34fea07d7581048063af2f56b6f3df9af74be
theskinnyhippo <134091490+theskinnyhippo@users.noreply.github.com>
2024-07-01 10:56:54 +0000

    opman

---
+++ 01_basics/04_operaction.js
@@ -0,0 +1,21 @@
+/*let s1 = "Mandir", s2 = " learns JS"
+
+console.log(s1 + s2)
+
+console.log(1 + "2")            // str + int => str + str(int)
+console.log("1" + 2)            // same 
+
+console.log(1 + "2" + 3)        // 123
+console.log(1 + 2 + "3")        // str(1+2) + "3" = 33
+console.log("1" + 2 + 3)        // 123
+
+console.log(+true)              //=> 1
+console.log(+"")                //=> 0
+//console.log(true+)              //=> error, no type conversion of operators
+
+console.log(" ");*/
+
+
+/***********************<conditionals>***********************/
+
+
