548409ee4225e2aa06ae44167f683d3cab5bf099
theskinnyhippo <134091490+theskinnyhippo@users.noreply.github.com>
2024-07-01 07:31:37 +0000

    vid 3 done and dusted

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict";   //treat all JS code as newer version
+
+//alert(3+3)     
+
+//we are using node.js not browser
+//in browser it shows popups.. which is basically an alert messege
+
+let age = 21
+let is_happy = true
+let state = null
+let dope
+
+// number => 2^53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => not defined variable
+// symbol => unique
+
+
+// object
+
+console.log(typeof state)       //null type is considered to be an object
+console.log(typeof is_happy)
+console.log(typeof age)
+console.log(typeof lol)     //undeclared => undefined
+console.log(typeof dope)    //undefined
+
--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -2,6 +2,7 @@ const acc_id = 65699556
 let acc_email = "mandirhapal@google.com"
 var acc_password = "14192"
 acc_city = "Hyderabad"
+let man
 
 //acc_id = 10833558     not allowed
 
@@ -10,7 +11,10 @@ console.log(acc_email)
 console.log(acc_password)
 console.log(acc_city)
 
-console.table([acc_id,acc_email,acc_password,acc_city])
+console.table([acc_id,acc_email,acc_password,acc_city, man])
 
 
 //js didnt have the control of scope --> {}
+//thats why when we use var, multiple definition of a single variable would be possible
+// because of issues with block scope and functional scope
+
