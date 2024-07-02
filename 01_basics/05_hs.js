471b095385a161e92c7d5614f187c5acb6967b24
theskinnyhippo <134091490+theskinnyhippo@users.noreply.github.com>
2024-07-02 08:06:39 +0000

    memory

---
+++ 01_basics/05_hs.js
@@ -0,0 +1,6 @@
+// memory 
+// stack( primitive data types)
+// heap( non primitive data types)
+
+// calling stack data.. only receives a copy of that data
+// calling heap data.. receives the ref of the original data
