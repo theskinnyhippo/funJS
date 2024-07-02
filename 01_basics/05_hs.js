151f2afef9274667a015f3edd75563f37a8d1197
theskinnyhippo <134091490+theskinnyhippo@users.noreply.github.com>
2024-07-02 09:09:22 +0000

    lol

--- 01_basics/05_hs.js
+++ 01_basics/05_hs.js
@@ -4,3 +4,11 @@
 
 // calling stack data.. only receives a copy of that data
 // calling heap data.. receives the ref of the original data
+
+// ===========================string interpolation==============================
+
+const name = "mandir"
+
+console.log(`haan ${name} chutiya hai`) //======> back ticks.. {``}
+
+
