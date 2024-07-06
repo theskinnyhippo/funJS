efef6e3226bc4bbae7373f79e378960cf85894b4
theskinnyhippo <134091490+theskinnyhippo@users.noreply.github.com>
2024-07-06 08:25:39 +0000

    obj2

--- 02_basics/02_obejcts.js
+++ 02_basics/02_obejcts.js
@@ -28,4 +28,44 @@ const man = {
 }
 
 
-console.log(man.head.face)
+console.log(man.feature.head)
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+const obj3 = { obj1, obj2 }
+console.log(obj3)
+
+//stores objects inside object ==> obj3
+
+const obj4 = Object.assign({}, obj1, obj2)
+console.log(obj4)         //==> target object
+
+
+const obj5 = { ...obj1 , ...obj2 }
+console.log(obj5)
+
+//spreadingly stores object keys into another object
+
+const USERS = [
+    {
+        id : 1,
+        email : "m@google.com"
+    },
+    {
+        id : 2,
+        email : "n@google.com"
+    },
+    {
+        id : 3,
+        email : "h@google.com"
+    }
+]
+console.log()
+console.log(Object.keys(tinderuser1))
+console.log(Object.values(tinderuser1))
+console.log(Object.entries(tinderuser1))
+
+
+console.log(tinderuser1.hasOwnProperty("name"))
