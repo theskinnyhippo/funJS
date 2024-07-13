5c44e7222254f67fa9a96338ee63910accd11da4
theskinnyhippo <134091490+theskinnyhippo@users.noreply.github.com>
2024-07-13 04:41:57 +0000

    switch

--- 06_small/1-buttons/magic.js
+++ 06_small/1-buttons/magic.js
@@ -1,3 +1,27 @@
 const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body")
 
-console.log(buttons)
+buttons.forEach(function(button){
+    console.log(button)
+    button.addEventListener('click', function(e){
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id === 'grey') {
+            body.style.backgroundColor = "rgb(14, 0, 78)";
+            body.style.color = "#fff";
+        }else
+        if(e.target.id === 'pearl') {
+            body.style.backgroundColor = "antiquewhite";
+            body.style.color = "rgb(58, 3, 3)";
+        }else
+        if(e.target.id === 'amber') {
+            body.style.backgroundColor = "rgb(255, 125, 197)";
+            body.style.color = "rgb(14, 0, 78)";
+        }else
+        if(e.target.id === 'honey') {
+            body.style.backgroundColor = "rgb(147, 83, 0)";
+            body.style.color = "rgb(123, 255, 0)";
+        }
+    })
+})
+
